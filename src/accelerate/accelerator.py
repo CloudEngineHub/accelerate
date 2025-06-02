8b03eadc1ef233034273d27d85e72e326546fdc4
S1ro1 <matej.sirovatka@gmail.com>
2025-06-02 10:33:37 +0000

    32 works

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1479,7 +1479,9 @@ class Accelerator:
             from torch.distributed.tensor.experimental import context_parallel
 
             device_mesh = DeviceMesh(
-                device_type="cuda", mesh=[[0, 1, 2, 3, 4, 5, 6, 7]], mesh_dim_names=["dp_shard", "cp"]
+                device_type="cuda",
+                mesh=[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]],
+                mesh_dim_names=["dp_shard", "cp"],
             )
             device_mesh["dp_shard", "cp"]._flatten("dp_shard_cp")
 
